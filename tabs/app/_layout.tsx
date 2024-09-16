3da2b8af6f873347ec3fbfc435651f02a8079fda
Hamza <myselfhamzaaziz@gmail.com>
2024-09-16 21:59:26 +0500

    added tabs with icons

--- tabs/app/_layout.tsx
+++ tabs/app/_layout.tsx
@@ -1,24 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { FontAwesome } from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 
 const RootLayout = () => {
   return (
     <Tabs>
+
       <Tabs.Screen name='index' options={{
         title: 'Home',
+        tabBarIcon: ({ color, focused, size }) => {
+          // return <FontAwesome name='home' color={color} size={size} />
+          // return <MaterialIcons name={focused ? 'home-filled' : 'home'} color={color} size={size} />
+          return <Ionicons name={ focused ? 'home' : 'home-outline' } color={color} size={size} />
+        },
+        headerShown: true
       }} />
       
       <Tabs.Screen name='about' options={{
-        title: 'About',
-        tabBarIcon: ({ color, size }) => (
+        title: 'About',  
+        tabBarIcon: () => (
           <View style={{
             backgroundColor: 'red',
             width: 20,
             height: 20,
             borderRadius: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
           }}>
           </View>
         ),
@@ -29,7 +37,19 @@ const RootLayout = () => {
         },
       }} />
 
-      <Tabs.Screen name='contact' />
+      <Tabs.Screen name='contact' options={{
+        title: 'Contact',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name='call-outline' color={'green'} size={size} />
+          // <MaterialIcons name='contact-mail' color={color} size={size} />
+        ),
+      }} />
+
+      <Tabs.Screen name='details' options={{
+        // When we want route to exist but not get showed in the tab bar
+        href: null
+      }} />
+
     </Tabs>
   )
 }
--- tabs/app/index.tsx
+++ tabs/app/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const Index = () => {
   return (
     <View>
       <Text>Index</Text>
+      <Link href={'/details'}>Go to Details</Link>
     </View>
   )
 }
