b1c88d40651c236195604652b7e607b95405c7f8
Hamza <myselfhamzaaziz@gmail.com>
2024-09-16 19:56:10 +0500

    added basic tabs

--- tabs/app/_layout.tsx
+++ tabs/app/_layout.tsx
@@ -1,12 +1,37 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
-const _layout = () => {
+const RootLayout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+    <Tabs>
+      <Tabs.Screen name='index' options={{
+        title: 'Home',
+      }} />
+      
+      <Tabs.Screen name='about' options={{
+        title: 'About',
+        tabBarIcon: ({ color, size }) => (
+          <View style={{
+            backgroundColor: 'red',
+            width: 20,
+            height: 20,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          </View>
+        ),
+        tabBarLabel: 'About Tab',
+        tabBarLabelStyle: {
+          color:'red',
+          fontWeight: 'bold',
+        },
+      }} />
+
+      <Tabs.Screen name='contact' />
+    </Tabs>
   )
 }
 
-export default _layout
+export default RootLayout
